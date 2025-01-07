7a309a512330fac16b86fe05bccf991d52d5fecb
Andres Paracare <andresparacare00@gmail.com>
2025-01-06 23:58:05 -0400

    test completed

--- windows/gui/utility/animation.py
+++ windows/gui/utility/animation.py
@@ -2,7 +2,7 @@ import numpy as np
 from windows.gui.utility.custom_window import color_pallete #Function for color pallete
 
 class animation():
-    def __init__(self, ax):
+    def __init__(self):
         """Create animation of a chart"""
         self._x = 0
         self._y = 0
@@ -10,9 +10,10 @@ class animation():
         self.example()
 
         #Modificar color en el argumento c
-        self.line2 = ax.plot(self._x[0], self._y[0], label=f'v0 = {self._y[1:]/self._x[1:]} m/s')[0]
-        ax.set(xlim=[0, 3], ylim=[-4, 10], xlabel='Time [s]', ylabel='Z [m]')
-        ax.legend()
+        #self.ax is connect with graphTankModule
+        self.line2 = self.ax.plot(self._x[0], self._y[0], label=f'v0 = {self._y[1:]/self._x[1:]} m/s')[0]
+        self.ax.set(xlim=[0, 3], ylim=[-4, 10], xlabel='Time [s]', ylabel='Z [m]')
+        #self.ax.legend()
 
     def update(self, frame):
         """update the line plot"""
--- windows/gui/widget/graphTankModule.py
+++ windows/gui/widget/graphTankModule.py
@@ -1,19 +1,22 @@
 import matplotlib.pyplot as plt # Import pyplot
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg # Import FigureCanvasTkAgg for we work with canvas
-import matplotlib.animation as animation # animation
+import matplotlib.animation as animation_tools # animation
+from windows.gui.utility.animation import animation #Function for color pallete
 import customtkinter as ctk # Import customtkinter for custom widgets
 
+class graph_oil_window(animation):
+    def __init__(self, root):
+        fig, self.ax = plt.subplots(figsize=(12, 6.2))  # Change plt.subplot() to plt.subplots()
+        super().__init__()
 
-class graph_oil_window():
-    def __init__(self,root):
-        fig, ax = plt.subplots(figsize = (12, 6.2))  # Change plt.subplot() to plt.subplots()
-        ax.plot([1,2,3,4,5], [1,4,9,16,25])  # plotting the data
+        #Create animation
+        ani = animation_tools.FuncAnimation(fig=fig, func=self.update, frames=40, interval=30)
         
         # Create a Tkinter canvas
-        canvas = FigureCanvasTkAgg(fig, master = root) # Create the Tkinter canvas with the figure
+        canvas = FigureCanvasTkAgg(fig, master=root) # Create the Tkinter canvas with the figure
         canvas.draw() # Draw the canvas, the figure will be painted here
         canvas.get_tk_widget().pack(
-            side = ctk.TOP,
-            fill = ctk.BOTH,
-            expand = True
+            side=ctk.TOP,
+            fill=ctk.BOTH,
+            expand=True
         ) # Put the canvas in the Tkinter window
